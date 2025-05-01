7a795e667dcf0e5c8f8dd9999517b1104ee98ef9
Nikitahh <nikitahsieh03@gmail.com>
2025-05-02 04:33:31 +0800

    feature section

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,15 +1,23 @@
+import { twMerge } from "tailwind-merge";
+
 export default function FeatureCard(props: {
     title: string;
     description: string;
     children?: React.ReactNode;
+    className?: string;
 }) {
-    const { title, description, children } = props;
+    const { title, description, children, className } = props;
     return (
-        <div>
-            <div>{children}</div>
+        <div
+            className={twMerge(
+                "bg-neutral-900 botder border-white/10 p-6 rounded-3xl",
+                className
+            )}
+        >
+            <div className="aspect-video">{children}</div>
             <div>
-                <h3>{title}</h3>
-                <p>{description}</p>
+                <h3 className="text-3xl font-medium mt-6">{title}</h3>
+                <p className="text-white/50 mt-2 ">{description}</p>
             </div>
         </div>
     );
