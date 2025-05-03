d0a5d310335dc979fa8a16bb32aef9102711f2db
Nikitahh <nikitahsieh03@gmail.com>
2025-05-03 23:49:54 +0800

    cursor animation & interaction

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,19 +1,61 @@
+"use client";
 import Button from "@/components/Button";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
+import { delay, easeInOut, motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
+import userCursor from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
+    const [leftPointerScope, leftPointerAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+    useEffect(() => {
+        leftPointerAnimate([
+            [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+            [
+                leftPointerScope.current,
+                { x: 0, y: [0, 16, 0] },
+                { duration: 0.5, ease: "easeInOut" },
+            ],
+        ]);
+        rightPointerAnimate([
+            [
+                rightPointerScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightPointerScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+            [
+                rightPointerScope.current,
+                { x: 0, y: [0, 20, 0] },
+                { duration: 0.5 },
+            ],
+        ]);
+    }, []);
     return (
-        <section className="py-24">
+        <section
+            className="py-24"
+            style={{ cursor: `url(${userCursor.src}), auto` }}
+        >
             <div className="container">
                 <div className="flex justify-center">
                     {/* Pointer */}
-                    <div className="absolute left-56  top-96">
+                    <motion.div
+                        ref={leftPointerScope}
+                        initial={{ opacity: 0, y: 100, x: -200 }}
+                        className="absolute left-56  top-96"
+                    >
                         <Pointer name="Vic" />
-                    </div>
-                    <div className="absolute right-80 top-34">
+                    </motion.div>
+                    <motion.div
+                        ref={rightPointerScope}
+                        initial={{ opacity: 0, y: 275, x: 100 }}
+                        className="absolute right-80 top-34"
+                    >
                         <Pointer name="Bryan" color="red" />
-                    </div>
+                    </motion.div>
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨ $7.5M seed round raised
                     </div>
