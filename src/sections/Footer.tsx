74484c3f7f6681ad5d0b86742e74e490350c06e7
Nikitahh <nikitahsieh03@gmail.com>
2025-05-03 11:56:55 +0800

    cta & footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import logo from "@/assets/images/logo.svg";
+import { a } from "framer-motion/client";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +9,27 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16 ">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <div>
+                        <Image src={logo} alt="logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    href={link.href}
+                                    className="text-white/50 text-sm"
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
